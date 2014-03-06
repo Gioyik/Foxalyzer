2393779611aacbc6ba21b025e4a588f58491e91c
Christiane Ruetten <cr@mozilla.com>
2014-03-06 01:04:36 +0100

    Squashed 'modules/retire.js/' content from commit a41d67d
    git-subtree-dir: modules/retire.js
    git-subtree-split: a41d67d8a136fb2fc53cbef7c92f1fafc5cbb2ea

---
+++ node/lib/resolve.js
@@ -0,0 +1,53 @@
+/* global require, exports */
+
+var walkdir			= require('walkdir'),
+	fs				= require('fs'),
+	readInstalled	= require('read-installed'),
+	emitter			= require('events').EventEmitter;
+
+
+function listdep(parent, filter, dep, level, deps) {
+	for (var i in dep.dependencies) {
+		if (filter !== null && filter.indexOf(i) == -1) {
+			continue;
+		}
+		var d = { component: i, version: dep.dependencies[i].version, parent: parent, level: level };
+		deps.push(d);
+		listdep(d, null, dep.dependencies[i], level + 1, deps);
+	}
+}
+
+function getNodeDependencies(path, limit) {
+	var events = new emitter();
+	readInstalled(path, null, null, function (er, pkginfo) {
+		var deps = [];
+		var filter = null;
+		if (limit) {
+			var packages = JSON.parse(fs.readFileSync(path +'/package.json'));
+			filter = [];
+			for(var k in packages.dependencies) filter.push(k);
+		}
+		listdep({component: pkginfo.name, version: pkginfo.version}, filter, pkginfo, 1, deps);
+		events.emit('done', deps);				
+	});
+	return events;
+}
+
+function scanJsFiles(path) {
+	var finder = walkdir.find(path);
+	finder.on('file', function (file) {
+		if (file.match(/\.js$/)) {
+			finder.emit('jsfile', file);
+		}
+	});
+	return finder;
+}
+
+exports.scanJsFiles = function(path) {
+	return scanJsFiles(path);
+};
+
+exports.getNodeDependencies = function(path, limit) {
+	return getNodeDependencies(path, limit);
+};
+
---
+++ node/lib/scanner.js
@@ -0,0 +1,86 @@
+var retire = require('./retire'),
+    _      = require('underscore'),
+    fs     = require('fs'),
+    crypto = require('crypto'),
+    log    = require('./utils').log,
+    emitter   = new require('events').EventEmitter;
+
+var events = new emitter();
+
+var hash = {
+  'sha1' : function(data) {
+    shasum   = crypto.createHash('sha1');
+    shasum.update(data);
+    return shasum.digest('hex');
+  }
+};
+
+function printResults(file, results, options) {
+  if (!retire.isVulnerable(results) && !options.verbose) return;
+  var logger = log(options).info;
+  if (retire.isVulnerable(results)) {
+    logger = log(options).warn;
+    events.emit('vulnerable-dependency-found');
+  }
+  if (results.length > 0) {
+    logger(file);
+    var printed = {};
+    results.forEach(function(elm) {
+      var vuln = '';
+      var key = elm.component + ' ' + elm.version;
+      if (printed[key]) return;
+      if (retire.isVulnerable([elm])) {
+        vuln = ' has known vulnerabilities: ' + elm.vulnerabilities.join(' ');
+      }
+      logger(' ' + String.fromCharCode(8627) + ' ' + key + vuln);
+      printed[key] = true;
+    });
+  }
+}
+function shouldIgnore(file, ignores) {
+  return _.detect(ignores, function(i) { return file.indexOf(i) === 0; });
+}
+
+
+function scanJsFile(file, repo, options) {
+  if (options.ignore && shouldIgnore(file, options.ignore)) {
+    return;
+  }
+  var results = retire.scanFileName(file, repo);
+  if (!retire.isVulnerable(results)) {
+    results = retire.scanFileContent(fs.readFileSync(file), repo, hash);
+  }  
+  printResults(file, results, options);
+}
+
+function printParent(comp, options) {
+  if ('parent' in comp) printParent(comp.parent, options);
+  log(options).info(new Array(comp.level).join(' ') + (comp.parent ? String.fromCharCode(8627) + ' ' : '') + comp.component + ' ' + comp.version);
+}
+
+function scanDependencies(dependencies, nodeRepo, options) {
+    for (var i in dependencies) {
+		results = retire.scanNodeDependency(dependencies[i], nodeRepo);
+		if (retire.isVulnerable(results)) {
+			events.emit('vulnerable-dependency-found');
+			var result = results[0]; //Only single scan here
+			log(options).warn(result.component + ' ' + result.version + ' has known vulnerabilities: ' + result.vulnerabilities.join(' '));
+			if (result.parent) {
+				printParent(result, options);
+			}
+		}
+    }
+}
+
+
+exports.scanDependencies = function(dependencies, nodeRepo, options) {
+	return scanDependencies(dependencies, nodeRepo, options);
+};
+exports.scanJsFile = function(file, repo, options) {
+	return scanJsFile(file, repo, options);
+};
+exports.on = function(name, listener) {
+	events.on(name, listener);
+};
+
+
